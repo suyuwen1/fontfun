0cc213c21fbc07de451460816b5c9ea68669925a
suyuwen1 <suyuwen1@126.com>
2015-06-12 17:02:56 +0800

    2015.6.12

--- test.php
+++ test.php
@@ -8,14 +8,17 @@
 <meta name="keywords" content="">
 <link href="" rel="stylesheet">
 <style type="text/css">
-	span{
-		display: inline-block;
+	textarea{
 		margin: 0;
-		padding: 0 5px;
+		padding: 0;
+		border: none;
+		width: 100%;
+		height: 600px;
+		font-size: 12px;
 	}
 </style>
 </head>
-<body style="font-size:10px;">
+<body style="font-size:24px;">
     <?php
 /**
  * 读取汉字点阵数据
@@ -25,7 +28,7 @@
  * @Copyright www.ugia.cn  
  */
 
-$str = "欢迎来say588!";
+$str = "中国民423142134人14234民1234共和斯拉@@夫@$%^*&(*)(dasg好！fafafasdfasdf。13241234.Welcome!";
 $str = iconv('UTF-8', 'GB2312', $str);
 
 //echo strlen($str).'<br>';
@@ -73,16 +76,61 @@ fclose($fp);
 $str = iconv('GB2312', 'UTF-8', $str);
 $rows=str_split($dot_string,$font_width);
 
+//echo mb_strlen($str,'utf8');
+
 $b_str='';
-for ($i=0; $i < $font_height; $i++) {
-	$n=$i;
-	for ($r=0; $r < mb_strlen($str,'utf8'); $r++) {
-		$b_str.=str_replace('1','说',str_replace('0', '　', $rows[$n])).'　';
-		$n+=$font_height;
+
+$n=0;
+
+$w=5;//一行5个字
+
+$h=ceil(count($rows)/($font_height*$w));//共几行
+
+$sn=mb_strlen($str,'utf8');//总字数
+
+for ($c=0; $c < $h; $c++) {
+	$nn=$n=$c*$w*12;
+	$w=($c==($h-1)&&($sn-$c*$w)!=0)? ($sn-$c*$w) : $w;
+	for ($i=0; $i < $font_height; $i++) {
+		//var_dump($w);
+		for ($r=0; $r < $w; $r++) {
+			$b_str.=str_replace('1','说',str_replace('0', '　', $rows[$n])).'　';
+			$n+=$font_height;
+		}
+		$n=($c==0)? ($i+1) : ($i+$nn);
+		$b_str.="\r\n";
 	}
 	$b_str.="\r\n";
 }
-echo '<textarea readonly style="height:400px;width:100%;">'.$b_str.'</textarea>';
+
+
+
+// for ($i=0; $i < 12; $i++) {
+// 	$n=$i;
+// 	for ($r=0; $r < 10; $r++) {
+// 		$b_str.=$n.'　　　';
+// 		$n+=12;
+// 	}
+// 	$b_str.="<br>";
+// }
+
+// echo $b_str;
+
+
+
+
+
+
+// for ($i=0; $i < $font_height; $i++) {
+// 	$n=$i;
+// 	for ($r=0; $r < mb_strlen($str,'utf8'); $r++) {
+// 		$b_str.=str_replace('1','说',str_replace('0', '　', $rows[$n])).'　';
+// 		$n+=$font_height;
+// 	}
+// 	$b_str.="\r\n";
+// }
+echo '<textarea readonly>'.$b_str.'</textarea>';
+//echo '<textarea readonly>'.$b_str.'</textarea>';
 ?>
 </body>
 </html>
